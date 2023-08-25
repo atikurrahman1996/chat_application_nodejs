8938aabe59d02a1dd50d76e913ed3030508eb707
atik <atikur2013rahman@gmail.com>
2023-08-25 22:47:57 +0600

    chat application

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,7 +1,20 @@
 const express = require("express");
-const { getUsers } = require("../controllers/usersController");
+const { getUsers, addUser } = require("../controllers/usersController");
+const avatarUpload = require("../middlewares/users/avatarUpload");
+const {
+  addUserValidators,
+  addUserValidationHandler,
+} = require("../middlewares/users/userValidators");
 const router = express.Router();
 
 router.get("/", getUsers);
+
+router.post(
+  "/",
+  avatarUpload,
+  addUserValidators,
+  addUserValidationHandler,
+  addUser
+);
 
 module.exports = router;
